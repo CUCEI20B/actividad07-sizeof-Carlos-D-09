125e5e55e9ba503a156ec8aaafca64bcc0e6264e
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2020-09-30 19:18:24 -0500

    Solicite el tipo de dato, realice las validacion para identificar cual es e implemente una impresion del tamaño en bytes del tipo de dato

--- main.cpp
+++ main.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+int main()
+{
 
+    string Tipo_Dato;
+
+    cin>>Tipo_Dato;
+
+    if (Tipo_Dato == "char")
+    {
+        cout<<endl<<sizeof(char)<<endl;
+    }
+    else if(Tipo_Dato=="int")
+    {
+        cout<<endl<<sizeof(int)<<endl;
+    }
+    else if(Tipo_Dato=="short")
+    {
+        cout<<endl<<sizeof(int)<<endl;
+    }
+    else if(Tipo_Dato=="float")
+    {
+        cout<<endl<<sizeof(int)<<endl;
+    }
+    else if(Tipo_Dato=="double")
+    {
+        cout<<endl<<sizeof(int)<<endl;
+    }
+    else if(Tipo_Dato=="long")
+    {
+        cout<<endl<<sizeof(int)<<endl;
+    }
+    else
+    {
+        cout<<endl<<"No es un tipo de dato";
+    }
     return 0;
 }
